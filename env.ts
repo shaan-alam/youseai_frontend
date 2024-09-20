5461623662447fcc50dc4cc79439a40ae1985c6c
shaan-m0 <shaan@m0.ventures>
2024-09-20 20:24:26 +0530

    fixed env errors

--- env.ts
+++ env.ts
@@ -6,7 +6,6 @@ export const env = createEnv({
     NEXT_PUBLIC_SERVER_BASE_URL: z.string(),
   },
   runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_SERVER_BASE_URL: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
   },
 });
